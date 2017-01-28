23eb7c1f2e81268feb680d35c115a23b4dec2936
Denis Mihaylov <denis.mihaylov93@gmail.com>
2017-01-28 11:59:36 +0200

    Creates initial project structure

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PokerBot",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Denis Mihaylov, Stoyan Eftimov",
+    author_email="denis.mihaylov93@gmail.com",
+    description="This bot that plays poker",
+    license="GNU",
+    keywords="pokerbot",
+    url="https://github.com/denisMihaylov/PokerBot",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Email',
+        'Topic :: Software Development :: Bug Tracking',
+    ],
+)
