4d7a5b9fc3c3084f37db76af658c0ccd8adda7f4
Denis Mihaylov <denis.mihaylov93@gmail.com>
2017-02-16 00:46:19 +0200

    Initial AI commit

--- play_game.py
+++ play_game.py
@@ -1,14 +1,18 @@
 from pokerbot.poker.poker import Poker
 from pokerbot.ai.aiplayers import MonteCarloAI
 import logging
+import time
 
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger("poker-console")
 LOGGER.setLevel(logging.INFO)
 
+start = time.time()
 players = [
     MonteCarloAI("Player 1", 1000),
-    MonteCarloAI("Player 2", 1000)
+    MonteCarloAI("Player 2", 1000),
+    MonteCarloAI("Player 3", 1000),
+    MonteCarloAI("Player 4", 1000)
 ]
 
 game = Poker(players)
@@ -16,3 +20,4 @@ winner = game.play()
 
 LOGGER.info("Winner is :%s" % winner)
 LOGGER.info("GAME ENDED SUCCESSFULLY")
+LOGGER.info("Elasped Time: %d s." % (time.time() - start))
--- pokerbot/ai/aiplayers.py
+++ pokerbot/ai/aiplayers.py
@@ -5,6 +5,8 @@ from pokerbot.poker.player import Call, Fold, Bet, Check
 from pokerbot.ai import utils
 import logging
 from pokerbot.poker.deck import Card
+import numpy
+import itertools
 
 LOGGER = logging.getLogger('poker-ai-players1')
 
@@ -63,4 +65,43 @@ class MonteCarloAI(BasePlayer):
         return Fold(self, round_)
 
     def get_amount(self, _min, _max):
-        return random.randint(_min, _max);
+        return random.randint(_min, _max)
+
+
+MAX_OPPENENTS = 5
+
+pre_flop_table = numpy.zeros((169, MAX_OPPENENTS))
+
+def is_increasing(l):
+    return all(a <= b for a, b in zip(l[:-1], l[1:]))
+
+def get_all_possible_flops(num_cards):
+    all_flops = list(itertools.permutations(range(2, 15), num_cards))
+    return filter(is_increasing, all_flops)
+
+
+
+
+def init_post_flop_table():
+    if post_flop_table_initialized:
+        return
+    flop = 3 * [0]
+    flop_id = 0
+    for full_flop in get_all_possible_flops(5):
+        flop = full_flop
+        m = full_flop[3]
+        n = full_flop[4]
+        hole_id = 0
+        for m in range(2, 29):
+            continue if m in flop
+            for n in range(m + 1, )
+            if m < 14:
+                pass
+
+
+class NeuralAI(BasePlayer):
+
+    NAME = "NeuralAI"
+
+    def __init__(self, name, starting_mone)
+        pass
